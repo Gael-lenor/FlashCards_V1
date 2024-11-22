74718339fb761211319c66c51fac95fa679f8a6b
Gael-lenor <galgr1000@gmail.com>
2024-11-22 10:21:23 -0300

    Add files via upload

---
+++ FlashCard/perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Astronomia',
+    'Qual planeta um do nosso sistema solar um dia possuiu um anel?',
+    'Marte! talvez a muito tempo atrás'
+)
+
+criaCartao(
+    'Biologia',
+    'Passiflora edulis é o nome científico dado a qual planta?',
+    'Os maracujazeiros, cuja a flor tem uma beleza incrível'
+)
